4c95072716e19206fa4434e9a96f32ef0129b5fd
先﨑生真 <ikuma.zakky@gmail.com>
2022-08-27 01:12:38 +0900

    改善と解説コメントアウト少し

--- calendar.js
+++ calendar.js
@@ -1,19 +1,29 @@
-const week = ["日", "月", "火", "水", "木", "金", "土"];//曜日
-const today = new Date();
-// 月末だとずれる可能性があるため、1日固定で取得
+const week = ["日", "月", "火", "水", "木", "金", "土"];//曜日を入れた配列を使う
+const today = new Date();//現在の日付を取る
+
 var showDate = new Date(today.getFullYear(), today.getMonth(), 1);
+// 今月の１日を取得(月末だとずれる可能性があるため)
+/*
+Date(年,月,日,時,分,秒,ミリ秒)のかたちでミリ秒を指定できる。
+日以降は省略可能で、その場合１日の0:00 0.000秒になる
+*/
 
 // 初期表示
 window.onload = function () {
   showProcess(today, calendar);
+  /*ShowProcessは処理モーダル（モーダルは操作するまで親ウィンドウを操作できなくする小ウィンドウ）を表示する関数
+  
+  */
 };
-// 前の月表示
+
+//window.onloadはHTMLが読み込まれてすぐに実行される関数
+// 前の月表示(＜ボタン)
 function prev() {
   showDate.setMonth(showDate.getMonth() - 1);
   showProcess(showDate);
 }
 
-// 次の月表示
+// 次の月表示(＞ボタン)
 function next() {
   showDate.setMonth(showDate.getMonth() + 1);
   showProcess(showDate);
@@ -22,11 +32,18 @@ function next() {
 // カレンダー表示
 function showProcess(date) {
   var year = date.getFullYear();
-  var month = date.getMonth();
-  document.querySelector('#header').innerHTML = year + "年 " + (month + 1) + "月";
+  var month = date.getMonth();//１月が０で返されるので+1する必要あり
+  document.getElementById('header').innerHTML = year + "年 " + (month + 1) + "月";
+  /*
+  querySelectorはidでもclassでも指定して要素受け取れる
+  #〜ならidで、.〜ならclassで持ってこれる
+  ただしquerySelectorは１つ目の要素しか持ってこれない
+  ただ遅いから、idならgetElementById使ったほうがいいらしい
+  ということで改変しました
+  */
 
   var calendar = createProcess(year, month);
-  document.querySelector('#calendar').innerHTML = calendar;
+  document.getElementById('calendar').innerHTML = calendar;
 }
 
 // カレンダー作成
