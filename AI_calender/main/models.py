30f05e4256030c23b374905dc70a537cb4a30bc6
kotama7 <kotamatakanori2@gmail.com>
2022-08-29 17:59:32 +0900

    データベース構築

--- AI_calender/main/models.py
+++ AI_calender/main/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from datetime import date
 
 # Create your models here.
+class schedule(models.Model):
+    date = models.DateField()
+    description = models.CharField()
+    details = models.CharField()
+    mode = models.CharField()
+
+    def __str__(self):
+        return self.description
+
+class Individual_data(models.Model):
+    name = models.CharField()
+    password = models.CharField()
+    data = models.ForeignKey(schedule,on_delete=models.CASCADE)
