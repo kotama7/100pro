59001214c91af445fe8fbe85d42cda64e3c81f4e
kotama7 <kotamatakanori2@gmail.com>
2022-08-29 18:04:11 +0900

    データベース構築

--- AI_calender/main/models.py
+++ AI_calender/main/models.py
@@ -4,14 +4,14 @@ from datetime import date
 # Create your models here.
 class schedule(models.Model):
     date = models.DateField()
-    description = models.CharField()
-    details = models.CharField()
-    mode = models.CharField()
+    description = models.CharField(max_length=100)
+    details = models.CharField(max_length=1000)
+    mode = models.CharField(max_length=100)
 
     def __str__(self):
         return self.description
 
 class Individual_data(models.Model):
-    name = models.CharField()
-    password = models.CharField()
+    name = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
     data = models.ForeignKey(schedule,on_delete=models.CASCADE)
