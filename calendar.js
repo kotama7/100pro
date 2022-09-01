1172d653aec10a2b7bf8d0969a9bb7983eaccd3d
先﨑生真 <ikuma.zakky@gmail.com>
2022-09-01 15:18:43 +0900

    コメントアウトを追加、GoogleCalendarのAPIを使いたくて、コメントアウトの方式でコピペしてみた

--- calendar.js
+++ calendar.js
@@ -7,9 +7,39 @@ var showDate = new Date(today.getFullYear(), today.getMonth(), 1);
 Date(年,月,日,時,分,秒,ミリ秒)のかたちでミリ秒を指定できる。
 日以降は省略可能で、その場合１日の0:00 0.000秒になる
 */
+//APIで祝日を取得
+const calendarId = 'ja.japanese#holiday@group.v.calendar.google.com'
+const calendarApiUrl = 'https://www.googleapis.com/calendar/v3/calendars/'
+const apiKey = 'APIキー'
+ 
+/*function loadGoogleCalendarApi() {
+  gapi.client.init({
+    apiKey: apiKey,
+  }).then(() => {
+    return gapi.client.request({
+      path: calendarApiUrl + encodeURIComponent(calendarId) + '/events'
+    })
+  }).then((res) => {
+    const items = (res.result.items).reduce((a, c) => {
+      a.push({date: c.start.date, summary: c.summary})
+      return a
+    }, []).sort((a, b) => {
+      if (new Date(a.date) > new Date(b.date)) {
+        return 1
+      } else if (new Date(a.date) < new Date(b.date)) {
+        return -1
+      } else {
+        return 0
+      }
+    })
+    console.table(items)
+  })
+}
+gapi.load('client', loadGoogleCalendarApi)*/
 
 // 初期表示
 window.onload = function () {
+  //loadGoogleCalendarApi()
   showProcess(today, calendar);
 };
 
@@ -44,27 +74,26 @@ function showProcess(date) {
 }
 
 // カレンダー作成
-function createProcess(year, month) {
-  // 曜日
+function createProcess(year, month) {//カレンダーを作る関数
   var calendar = "<table><tr class='dayOfWeek'>";
   for (var i = 0; i < week.length; i++) {
     calendar += "<th>" + week[i] + "</th>";
   }
-  calendar += "</tr>";
+  calendar += "</tr>";//ここまでで表の見出しthタグとして曜日をかく
 
-  var count = 0;
-  var startDayOfWeek = new Date(year, month, 1).getDay();
-  var endDate = new Date(year, month + 1, 0).getDate();
-  var lastMonthEndDate = new Date(year, month, 0).getDate();
-  var row = Math.ceil((startDayOfWeek + endDate) / week.length);
+  var count = 0;//日付のカウント
+  var startDayOfWeek = new Date(year, month, 1).getDay();//表示する月の1日の曜日
+  var endDate = new Date(year, month + 1, 0).getDate();//表示する月の末日の曜日
+  var lastMonthEndDate = new Date(year, month, 0).getDate();//表示する月の前の月の末日
+  var row = Math.ceil((startDayOfWeek + endDate) / week.length);//カレンダーの行数
 
   // 1行ずつ設定
   for (var i = 0; i < row; i++) {
-    calendar += "<tr>";
-    // 1colum単位で設定
+    calendar += "<tr>";//表のtrタグの始まりを作る。
+    // 日付単位で設定
     for (var j = 0; j < week.length; j++) {
       if (i == 0 && j < startDayOfWeek) {
-        // 1行目で1日まで先月の日付を設定
+        // 1行目で1日まで先月の日付を設定(j<startDayOfWeekは１日の前までにするためにある)
         calendar += "<td class='disabled'>" + (lastMonthEndDate - startDayOfWeek + j + 1) + "</td>";
       } else if (count >= endDate) {
         // 最終行で最終日以降、翌月の日付を設定
@@ -72,6 +101,7 @@ function createProcess(year, month) {
         calendar += "<td class='disabled'>" + (count - endDate) + "</td>";
       } else {
         // 当月の日付を曜日に照らし合わせて設定
+        //今日かそうじゃないかで分けて入れる（色分けのため）
         count++;
         if (year == today.getFullYear()
           && month == (today.getMonth())
